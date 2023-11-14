ea5bf202842002367e64d2d4e3b47419733555a6
denisrep <Repkinden04@yandex.ru>
2023-11-14 19:00:14 +0400

    Добавьте файлы проекта.

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : Shell
+    {
+        public MainPage()
+        {
+            // Устанавливаем ширину бокового меню
+            FlyoutWidth = 50;
+
+            Items.Add(new FlyoutItem
+            {
+                // Указываем путь к иконке
+                Icon = "icon_globe.png",
+                Items = {
+                    new Tab {
+                        Items = { new ShellContent { Content = new ContentPage()} }
+                    }
+                }
+            }) ;
+            Items.Add(new FlyoutItem
+            {
+                Icon = "icon_globe.png",
+                Items =
+                {
+                    new Tab
+                    {
+                        Items = { new ShellContent {Content = new StartPage()} }
+                    }
+                }
+            });
+            Items.Add(new FlyoutItem
+            {
+                Icon = "icon_globe.png",
+                Items =
+                {
+                    new Tab
+                    {
+                        Items = { new ShellContent {Content = new ContentPage()} }
+                    }
+                }
+            });
+            Items.Add(new FlyoutItem
+            {
+                Icon = "icon_globe.png",
+                Items =
+                {
+                    new Tab
+                    {
+                        Items = { new ShellContent {Content = new ContentPage()} }
+                    }
+                }
+            });
+            Items.Add(new FlyoutItem
+            {
+                Icon = "icon_globe.png",
+                Items =
+                {
+                    new Tab
+                    {
+                        Items = { new ShellContent {Content = new ContentPage()} }
+                    }
+                }
+            });
+            Items.Add(new FlyoutItem
+            {
+                Icon = "icon_globe.png",
+                Items =
+                {
+                    new Tab
+                    {
+                        Items = { new ShellContent {Content = new ContentPage()} }
+                    }
+                }
+            });
+            Items.Add(new FlyoutItem
+            {
+                Icon = "icon_globe.png",
+                Items =
+                {
+                    new Tab
+                    {
+                        Items = { new ShellContent {Content = new ContentPage()} }
+                    }
+                }
+            });
+            Items.Add(new FlyoutItem
+            {
+                Icon = "icon_globe.png",
+                Items =
+                {
+                    new Tab
+                    {
+                        Items = { new ShellContent {Content = new ContentPage()} }
+                    }
+                }
+            });
+            Items.Add(new FlyoutItem
+            {
+                Icon = "icon_globe.png",
+                Items =
+                {
+                    new Tab
+                    {
+                        Items = { new ShellContent {Content = new ContentPage()} }
+                    }
+                }
+            });
+            Items.Add(new FlyoutItem
+            {
+                Icon = "icon_globe.png",
+                Items =
+                {
+                    new Tab
+                    {
+                        Items = { new ShellContent {Content = new ContentPage()} }
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ App2/App2/StartPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App2
+{
+    class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            Button alertButton = new Button
+            {
+                Text = "Alert",
+                VerticalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.Center
+            };
+            alertButton.Clicked += AlertButton_Clicked;
+
+            Content = alertButton;
+        }
+
+        private void AlertButton_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Уведомление", "Пришло новое сообщение", "ОK");
+        }
+    }
+}
